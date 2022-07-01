9e38daeb4790ed3da46c35c2606e0ba7015063de
since123 <1534147975@qq.com>
2022-07-01 18:10:59 +0800

    上一首，下一首

--- pages/myCollections/myCollections.vue
+++ pages/myCollections/myCollections.vue
@@ -8,21 +8,23 @@
 		<view class="song-con">
 			<view class='song-list'>
 				<view class="play-btn">
-					<u-button class="play-btn" type="primary" color="#fff" text='播放全部' icon="play-circle-fill"></u-button>
+					<u-button class="play-btn" type="primary" color="#fff" text='播放全部' icon="play-circle-fill">
+					</u-button>
 				</view>
 				<view class='song-btn'>
 					<u-button type="primary" color="#fff" icon="list-dot"></u-button>
-					<u-button type="primary" color="#fff"icon="arrow-downward"></u-button>
+					<u-button type="primary" color="#fff" icon="arrow-downward"></u-button>
 					<u-button type="primary" color="#fff" icon="share-square"></u-button>
 				</view>
 			</view>
 			<view class='song-list' v-for="song in songList" :key='song._id'>
 				<view class="play-btn">
-					<u-button  type="primary" color="#fff" :text='song.title' @click='handleSongClick(song._id)'></u-button>
+					<u-button type="primary" color="#fff" :text='song.title' @click='handleSongClick(song._id)'>
+					</u-button>
 				</view>
 				<view class='song-btn'>
 					<u-button type="primary" color="#fff" icon="eye"></u-button>
-					<u-button type="primary" color="#fff"icon="download"></u-button>
+					<u-button type="primary" color="#fff" icon="download"></u-button>
 					<u-button type="primary" color="#fff" icon="more-dot-fill"></u-button>
 				</view>
 			</view>
@@ -34,98 +36,91 @@
 	export default {
 		data() {
 			return {
-				collectionList: [
-					{
-						name: '歌曲83',
-					}, {
-						name: '专辑4',
-					}, {
-						name: '歌单21'
-					}, {
-						name: '视频3'
-					},
-				],
-				songList:[
-					{
-						id: '1',
-						name: '错乱底线',
-						
-					},
-					{
-						id: '2',
-						name: '扎心',
-						
-					}
-				],
+				collectionList: [{
+					name: '歌曲83',
+				}, {
+					name: '专辑4',
+				}, {
+					name: '歌单21'
+				}, {
+					name: '视频3'
+				}, ],
+				songList: [],
 				loading: false
 			}
 		},
 		methods: {
 			list(e) {
-			  this.loading = true
-			  uni.showLoading({
-			      title: '加载中'
-			  });
-			  uniCloud
-			    .callFunction({
-			      name: 'list',
-			      data: {
-			        limit: 300,
-			        offset: 0,
-			        keyWord: e ? e.detail.value : ''
-			      }
-			    })
-			    .then(res => {
-			      console.log(res.result.data, 0)
-			      
-			      this.songList = res.result.data
-			      uni.hideLoading()
-			      this.loading = false
-			    })
+				this.loading = true
+				uni.showLoading({
+					title: '加载中'
+				});
+				uniCloud
+					.callFunction({
+						name: 'list',
+						data: {
+							limit: 300,
+							offset: 0,
+							keyWord: e ? e.detail.value : ''
+						}
+					})
+					.then(res => {
+						console.log(res.result.data, 0)
+
+						this.songList = res.result.data
+						uni.hideLoading()
+						this.loading = false
+					})
 			},
-			handleSongClick(id){
+			handleSongClick(id) {
 				uni.navigateTo({
-				  url: `/pages/song/song?id=${id}`
+					url: `/pages/song/song?id=${id}`
 				})
 			}
 		},
-		created(){
+		created() {
 			this.list()
 		}
 	}
 </script>
 
 <style lang="scss" scoped>
-.my-collection{
-	.u-tabs {
-		background: #fff;
-		::v-deep .u-tabs__wrapper__nav__line {
-			background: #31c27c !important;
-		}
-	}
-	.song-con{
-		padding: 10px;
-		background: #fff;
-	    margin-top: 10px;
-		.song-list{
-			display: flex;
-			justify-content: space-between;
-			.play-btn {
-				justify-content: left;
-			}
-			::v-deep .u-icon__icon {
-				color : gray !important;
-			}
-			::v-deep .u-button__text {
-				color: gray;
-			}
-			.song-btn {
-				display: flex;
-				width:40%;
-			}
-		}
-		
-	}
-}
+	.my-collection {
+		.u-tabs {
+			background: #fff;
 
+			::v-deep .u-tabs__wrapper__nav__line {
+				background: #31c27c !important;
+			}
+		}
+	
+	.song-con {
+			padding: 10px;
+			background: #fff;
+			margin-top: 10px;
+
+			.song-list {
+				display: flex;
+				justify-content: space-between;
+
+				.play-btn {
+					justify-content: left;
+				}
+
+				::v-deep .u-icon__icon {
+					color: gray !important;
+				}
+
+				::v-deep .u-button__text {
+					color: gray;
+				}
+
+				.song-btn {
+					display: flex;
+					width: 40%;
+				}
+			}
+
+		}
+	}
 </style>
--- pages/song/song.vue
+++ pages/song/song.vue
@@ -1,7 +1,7 @@
 <template>
 	<view class='song-con' :style="{height: screenHeight + 'px' }">
 		<view class="image-con">
-			<u--image   id='img' class='image-shape' src="https://cdn.uviewui.com/uview/album/1.jpg" ></u--image>
+			<u--image id='img' class='image-shape' src="https://cdn.uviewui.com/uview/album/1.jpg"></u--image>
 		</view>
 		<view class="song-center">
 			<view class="song-left">
@@ -12,32 +12,41 @@
 					<u--text :text='song.quality'></u--text>
 				</view>
 				<view class='lyric' ref='lyric'>
-					<u--text :class="{each:true, choose: (index==lyricIndex)}" v-for='(item,key,index) in currentMUsicLyric' :text='item' :key='key'></u--text>
+					<u--text :class="{each:true, choose: (index==lyricIndex)}"
+						v-for='(item,key,index) in currentMUsicLyric' :text='item' :key='key'></u--text>
 				</view>
 			</view>
-			<view class="song-right" >
+			<view class="song-right">
 				<u-button :icon="song.isCollection ? 'heart' : 'heart-fill' "></u-button>
 			</view>
 		</view>
 		<view class='song-foot'>
 			<view class='slide-con'>
-				<view>{{format(current)}}</view>
-				<u-slider 
-					@moving="moveing"
-					@end="endMove"
-					v-model="slideWidth"
-					:unidirectionalDatatTransfer="true"
-				></u-slider>
+				<view> {{current == 0 ? '00:00' : format(current)}}</view>
+				<u-slider @moving="moveing" @end="endMove" v-model="slideWidth" :unidirectionalDatatTransfer="true">
+				</u-slider>
 				<view>{{format(duration)}}</view>
 			</view>
 			<view class='song-tool'>
-				<u-button icon='reload' ></u-button>
-				<u-button icon='rewind-left-fill' ></u-button>
-				<u-button :icon="paused ?   'play-circle-fill':'pause-circle-fill' " @click="audio.paused ? play() : audio.pause()"></u-button>
-				<u-button icon='rewind-right-fill' ></u-button>
-				<u-button icon='list' ></u-button>
+				<u-button icon='reload'></u-button>
+				<u-button icon='rewind-left-fill' @click='prev()'></u-button>
+				<u-button :icon="paused ?   'play-circle-fill':'pause-circle-fill' "
+					@click="audio.paused ? play() : audio.pause()"></u-button>
+				<u-button icon='rewind-right-fill' @click='next()'></u-button>
+				<u-button icon='list' @click='openList()'></u-button>
 			</view>
 		</view>
+		<u-popup :show="ifShowList" mode="bottom">
+			<view class="song-length">播放列表（{{songList.length}}首）</view>
+			<view v-for="item in songList" :key='item._id' class="song-list">
+				<div class='song-header'>
+					<view class="song-title">{{item.title}}</view>
+					<view class='song-author'>- {{item.author}}</view>
+				</div>
+				<u-button icon='close'></u-button>
+			</view>
+			<view class='song-close' @click='ifShowList = false'>关闭</view>
+		</u-popup>
 	</view>
 </template>
 
@@ -46,29 +55,32 @@
 	export default {
 		data() {
 			return {
-				song:{},
+				song: {},
 				screenHeight: '',
 				audio: uni.createInnerAudioContext(),
-				slideWidth:'',
+				slideWidth: '',
 				paused: true, //是否处于暂停状态
 				current: 0, //当前进度(s)
-			    duration: 0, //总时长(s),
+				duration: 0, //总时长(s),
 				currentMUsicLyric: {},
-				lyricIndex:0,
-				rotateval:0,
-			    seek: false, //是否处于拖动状态
-				Interval:null ,// 定时器
-				loadding:false,
+				lyricIndex: 0,
+				rotateval: 0,
+				seek: false, //是否处于拖动状态
+				Interval: null, // 定时器
+				loadding: false,
+				ifShowList: false,
+				songList: [],
+				songId: ''
 			}
 		},
 		onLoad(option) {
-		  this.read(option.id || '5f64c4cca2a89f21dbd4fd66')
+			this.songId = option.id
+			this.read(option.id || '5f64c4cca2a89f21dbd4fd66')
 		},
 		methods: {
 			endMove() {
 				this.play()
 				const pr = (this.slideWidth / 100) * this.duration
-				// this.current = pr
 				this.audio.seek(pr)
 			},
 			moveing() {
@@ -81,93 +93,171 @@
 			},
 			//返回prev事件
 			prev() {
-				this.slideWidth = 0
-				// this.$emit('prev')
+				// 找歌曲
+				let targetIndex = this.songList.findIndex((item) =>
+					item._id == this.songId
+				)
+				console.log('targetIndex', targetIndex)
+				if (targetIndex == 0) {
+					// 第一首的时候播放最后一首,循环
+					this.songId = this.songList[this.songList.length - 1]._id
+
+				} else {
+					this.songId = this.songList[targetIndex - 1]._id
+
+				}
+				this.currentMUsicLyric = {}
+				this.read(this.songId)
+				// 进度条清0
+				this.paused = true
+				// this.audio.play()
+				// 歌词到最上面
+				this.lyricIndex = 0
+				// 图片旋转
+				this.rotateval = 0
+				clearInterval(this.Interval)
+				this.Interval = null
+				// this.audio.play()
 			},
 			//返回next事件
 			next() {
-				this.slideWidth = 0
-				// this.$emit('next')
+				// 找歌曲
+				let targetIndex = this.songList.findIndex((item) =>
+					item._id == this.songId
+				)
+				console.log('targetIndex', targetIndex)
+				if (targetIndex == this.songList.length - 1) {
+					// 已经是最后一首播放第一首,循环
+					this.songId = this.songList[0]._id
+
+				} else {
+					this.songId = this.songList[targetIndex + 1]._id
+
+				}
+				this.currentMUsicLyric = {}
+				this.read(this.songId)
+				// 进度条清0
+				this.paused = true
+
+				// 歌词到最上面
+				this.lyricIndex = 0
+				// 图片旋转
+				this.rotateval = 0
+				clearInterval(this.Interval)
+				this.Interval = null
+				// this.audio.play()
 			},
-			
-			play(){
+
+			play() {
 				this.audio.play()
-				// this.rotate()
 			},
 			//格式化时长
 			format(num) {
-				if(num){
-					return '0'.repeat(2 - String(Math.floor(num / 60)).length) + Math.floor(Number(num) / 60) + ':' + '0'.repeat(2 - String(Math.floor(num % 60)).length) + Math.floor(num % 60)
-					
+				if (num) {
+					return '0'.repeat(2 - String(Math.floor(num / 60)).length) + Math.floor(Number(num) / 60) +
+						':' + '0'
+						.repeat(2 - String(Math.floor(num % 60)).length) + Math.floor(num % 60)
+
 				}
 			},
-			getLyric(encodedlyric){
+			getLyric(encodedlyric) {
 				let lyric = decodeURIComponent(escape(atob(encodedlyric)))
-				
+
 				// 处理歌词，转化成key为时间，value为歌词的对象
-				    let lyricArr = lyric.split('[').slice(1); // 先以[进行分割
-				     let lrcObj = {};
-				     lyricArr.forEach(item => {
-				         let arr = item.split(']');	// 再分割右括号
-				         // 时间换算成秒
-				         let m = parseInt(arr[0].split(':')[0])
-				         let s = parseInt(arr[0].split(':')[1])
-				         arr[0] = m*60 + s;
-				         if (arr[1] != '\n') { // 去除歌词中的换行符
-				             lrcObj[arr[0]] = arr[1];
-				         }
-				     })
-				     // 存储数据
-				     this.currentMUsicLyric =   lrcObj;
+				let lyricArr = lyric.split('[').slice(1); // 先以[进行分割
+				let lrcObj = {};
+				lyricArr.forEach(item => {
+					let arr = item.split(']'); // 再分割右括号
+					// 时间换算成秒
+					let m = parseInt(arr[0].split(':')[0])
+					let s = parseInt(arr[0].split(':')[1])
+					arr[0] = m * 60 + s;
+					if (arr[1] != '\n') { // 去除歌词中的换行符
+						lrcObj[arr[0]] = arr[1];
+					}
+				})
+				// 存储数据
+				this.currentMUsicLyric = lrcObj;
 			},
-			rotate(){
+			rotate() {
 				let that = this
-				if(!that.paused){
-					that.Interval=setInterval(function(){
-						var img=document.getElementById('img');
+				if (!that.paused) {
+					that.Interval = setInterval(function() {
+						var img = document.getElementById('img');
 						that.rotateval++;
-						img.style.transform='rotate('+that.rotateval+'deg)'
+						img.style.transform = 'rotate(' + that.rotateval + 'deg)'
 						img.style.transition = '0.1s linear'
-					},100)
-				}else {
+					}, 100)
+				} else {
 					clearInterval(that.Interval)
 					that.Interval = null
 				}
 			},
 			read(id) {
 				this.loading = true
+
 				uni.showLoading({
 					title: '加载中'
 				});
 				uniCloud
-				  .callFunction({
-					name: 'read',
-					data: { id }
-				  })
-				  .then(res => {
-					// console.log(res.result.data[0], 1)
-					this.song = res.result.data[0] || {}
-					console.log("this.song",this.song)
-					this.getLyric( this.song.lyric)
-					this.audio.src = this.song.src
-					
-					uni.setNavigationBarTitle({
-					  title: `${this.song.title} - ${this.song.author}`
+					.callFunction({
+						name: 'read',
+						data: {
+							id
+						}
 					})
-					this.loading = false
-					uni.hideLoading();
-				  })
-			},				
-			
-			
+					.then(res => {
+						this.song = res.result.data[0] || {}
+						console.log("this.song", this.song)
+						this.getLyric(this.song.lyric)
+						this.audio.src = this.song.src
+						uni.setNavigationBarTitle({
+							title: `${this.song.title} - ${this.song.author}`
+						})
+						this.loading = false
+						uni.hideLoading();
+					})
 			},
-			
-			mounted(){
+			list(e) {
+				this.loading = true
+				uni.showLoading({
+					title: '加载中'
+				});
+				uniCloud
+					.callFunction({
+						name: 'list',
+						data: {
+							limit: 300,
+							offset: 0,
+							keyWord: e ? e.detail.value : ''
+						}
+					})
+					.then(res => {
+						console.log(res.result.data, 0)
+
+						this.songList = res.result.data
+						uni.hideLoading()
+						this.loading = false
+					})
+			},
+			openList() {
+				console.log('open')
+				this.ifShowList = true
+			}
+		},
+
+		mounted() {
+			// 获取歌曲列表
+			this.list()
 			this.screenHeight = uni.getSystemInfoSync().windowHeight
 			this.audio.onCanplay(() => {
-				this.duration = this.audio.durationrotate
+				// this.duration = this.audio.durationrotate
 				// this.audio.src = this.src
-				console.log(this.audio.duration, '音频能够播放了')
+				// this.current = 0
+				this.current = this.audio.currentTime
+				// this.current = this.audio.currentTime
+				this.duration = this.audio.duration
+				console.log('音频能够播放了', this.current)
 				// this.audio.play()
 			})
 			//音频进度更新事件
@@ -184,7 +274,7 @@
 			//音频播放事件
 			this.audio.onPlay(() => {
 				console.log('音频播放')
-				this.paused = false	
+				this.paused = false
 				this.rotate()
 			})
 			// 音频暂停事件
@@ -199,7 +289,7 @@
 				this.seek = false
 				this.$forceUpdate()
 			})
-			// this.getLyric()	
+
 		},
 		watch: {
 			current(value) {
@@ -208,18 +298,17 @@
 						this.slideWidth = 100
 						return
 					}
-					// this.slideWidth = parseInt(((parseInt(value) / parseInt(this.duration)) * 100).toFixed(2))
 					this.slideWidth = Number(((value / this.duration) * 100).toFixed(4))
 					let i = 0
 					// 循环歌词对象
 					for (let key in this.currentMUsicLyric) {
-					// key表示歌词对象中的时间，如果key等于歌曲进度value，改变当前歌词进度		lyricIndex
+						// key表示歌词对象中的时间，如果key等于歌曲进度value，改变当前歌词进度		lyricIndex
 						// console.log("value",key,parseInt(value))
-						if (+key == parseInt(value) ) {
+						if (+key == parseInt(value)) {
 							this.lyricIndex = i;
 							// 当歌词进度大于5，即播放到了第5句歌词，开始滚动
 							if (i > 3) {
-							 this.$refs.lyric.$el.scrollTop =this.lyricIndex  * 30;
+								this.$refs.lyric.$el.scrollTop = this.lyricIndex * 30;
 							}
 						}
 						i++;
@@ -231,117 +320,187 @@
 </script>
 
 <style lang="scss" scoped>
-.song-con {
-	background-color: #fff;
-	padding: 10px 20px;
-	display: flex;
-	flex-direction: column;
-	.image-con {
+	.song-con {
+		background-color: #fff;
+		padding: 10px 20px;
 		display: flex;
-		flex: 0 0 auto;
-		justify-content: center;
-		align-items: center;
-		background:url('../../static/bg.jpeg');
-		background-position: center;
-		background-size: 100%;
-		width:100%;
-		height: 250px;
-		border-radius: 15px;
-		::v-deep .u-image {
-			width: 150px !important;
-			height: 150px !important;
-			.u-image__image {
+		flex-direction: column;
+
+		.image-con {
+			display: flex;
+			flex: 0 0 auto;
+			justify-content: center;
+			align-items: center;
+			background: url('../../static/bg.jpeg');
+			background-position: center;
+			background-size: 100%;
+			width: 100%;
+			height: 250px;
+			border-radius: 15px;
+
+			::v-deep .u-image {
 				width: 150px !important;
 				height: 150px !important;
-				border-radius: 50% !important;
+
+				.u-image__image {
+					width: 150px !important;
+					height: 150px !important;
+					border-radius: 50% !important;
+				}
 			}
 		}
-	}
-	.song-center {
-		display: flex;
-		flex: 1 0 auto;
-		justify-content: space-between;
-		padding: 10px 5px;
-		.song-left {
-			.song-name {
-				::v-deep .u-text__value {
-					font-size: 20px !important;
-					white-space: nowrap;
-					font-weight:600 !important;
-					padding:10px 0;
-				}
-			}
-			.song-info {
-				display: flex;
-				padding:10px 0;
-				::v-deep .u-text__value {
-					font-size: 13px !important;
-					white-space: nowrap;
-				}
-				.follew {
+
+		.song-center {
+			display: flex;
+			flex: 1 0 auto;
+			justify-content: space-between;
+			padding: 10px 5px;
+
+			.song-left {
+				.song-name {
 					::v-deep .u-text__value {
-						padding: 2px 4px;
-						margin: 0 5px;
-						border: 1px solid gray !important;
-						border-radius: 10px !important;
-						font-size: 12px !important;
+						font-size: 20px !important;
+						white-space: nowrap;
+						font-weight: 600 !important;
+						padding: 10px 0;
+					}
+				}
+
+				.song-info {
+					display: flex;
+					padding: 10px 0;
+
+					::v-deep .u-text__value {
+						font-size: 13px !important;
+						white-space: nowrap;
+					}
+
+					.follew {
+						::v-deep .u-text__value {
+							padding: 2px 4px;
+							margin: 0 5px;
+							border: 1px solid gray !important;
+							border-radius: 10px !important;
+							font-size: 12px !important;
+						}
+					}
+				}
+
+				.lyric {
+					// width: 400px;
+					height: 110px;
+					// position: absolute;
+					// top: 360px;
+					// left: 20px;
+					background-color: #fff;
+					// 滚动条
+					overflow: auto;
+					color: #ddd;
+					font-size: 10px;
+					font-weight: normal;
+					padding: 5px 10px;
+
+					.each {
+						height: 30px;
+						// border: 1px solid #000;
+						line-height: 30px;
+						text-align: center;
+
+					}
+
+					.choose ::v-deep .u-text__value {
+						height: 30px;
+						line-height: 30px;
+						font-size: 20px;
+						color: #31c27c !important;
 					}
 				}
 			}
-			.lyric {
-				// width: 400px;
-				height: 110px;
-				// position: absolute;
-				// top: 360px;
-				// left: 20px;
-				background-color: #fff;
-				// 滚动条
-				overflow: auto;
-				color: #ddd;
-				font-size: 10px;
-				font-weight: normal;
-				padding: 5px 10px;
-				.each {
-					height: 30px;
-					// border: 1px solid #000;
-					line-height: 30px;
-					text-align: center;
-					
-					}
-				.choose ::v-deep .u-text__value
-				{
-					height: 30px;
-					line-height: 30px;
-					font-size: 20px;
-					color: #31c27c !important;
+
+			.song-right {
+				padding: 10px;
+
+				.u-button {
+					border: none;
 				}
 			}
 		}
-		.song-right {
-			padding:10px;
-			.u-button {
-				border:none;	
+
+		.song-foot {
+			padding: 30px 0;
+
+			.slide-con {
+				display: flex;
+				justify-content: center;
+
+				.u-slider {
+					width: 80%;
+				}
+
+				::v-deep .uni-slider-track {
+					background: #31c27c !important;
+				}
+			}
+
+			.song-tool {
+				display: flex;
+
+				.u-button {
+					border: none;
+				}
 			}
 		}
-    }
-	.song-foot {
-		padding:30px 0;
-		.slide-con {
+
+		/deep/.u-popup__content {
 			display: flex;
-			justify-content: center;
-			.u-slider {
-				width:80%;
+			background-color: #2e2c2c !important;
+			opacity: 0.8;
+			padding: 15px 0 0 15px;
+
+			.song-length {
+				color: #fff;
+
 			}
-			::v-deep .uni-slider-track {
-				background:#31c27c !important;
+
+			.song-list {
+				display: flex;
+
+				border-bottom: 1px solid #3c3a3a;
+
+				.song-header {
+					display: flex;
+					align-items: center;
+					width: 90%;
+
+					.song-title {
+						font-size: 12px;
+						color: #fff;
+						padding-right: 10px;
+					}
+
+					.song-author {
+						color: #898686;
+						font-size: 8px;
+					}
+				}
+
+				.u-button {
+					width: 10%;
+					background-color: #2e2c2c;
+					opacity: 0.8;
+					border: none;
+
+					.uicon-close {
+						color: #898686 !important;
+					}
+				}
 			}
-		}
-		.song-tool {
-			display: flex;
-			.u-button {
-				border:none;	
+
+			.song-close {
+				padding: 20px;
+				text-align: center;
+				color: #fff
 			}
 		}
 	}
-}
 </style>
